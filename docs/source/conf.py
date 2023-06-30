dab4754a5313cef77e2f4d967bba440893d141d3
anapascual <pascual.aranda.ana@gmail.com>
2023-06-30 15:38:46 +0200

    Testing docs 5

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -21,7 +21,7 @@ release = '1.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx.ext.autodoc', 'sphinxcontrib.matlab']
+extensions = ['sphinx.ext.autodoc']
 
 templates_path = ['_templates']
 exclude_patterns = []
