40c1f2c56d2a352a19fe6c7e2ab0de6b82b33c38
anapascual <pascual.aranda.ana@gmail.com>
2023-07-02 17:40:27 +0200

    Radiomica
    exit

--- RadCNN/radiomics/dataset_filtered.m
+++
@@ -1,7 +0,0 @@
-function [mammo_original, mammo_filtered] = dataset_filtered(path)
-% Returns matlab matrix of the image, filtred with dwt2   
-    im = imread(path);
-    [cA1,cH1,cV1,cD1] = dwt2(im,'db2');
-    mammo_original = im;
-    mammo_filtered = cA1;
-end
